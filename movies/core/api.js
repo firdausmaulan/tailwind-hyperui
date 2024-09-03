64b75549e77aad93af61fe6bf70d05b49eaa79f1
maulana.firdaus <maulana.firdaus@advotics.com>
2024-09-03 19:42:26 +0700

    movies

--- movies/core/api.js
+++ movies/core/api.js
@@ -2,7 +2,7 @@
 
 // Define the base URL for the API
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Y2IwMjVmZWU0Y2JmYjc4NzQxNWM2M2Q1ZmE4NzU4MyIsInN1YiI6IjU3MmYyOWIzOTI1MTQxMDk1NjAwMDM0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YHN3Of32k7-B_c04YUQjg1eF_wX5Nmlu5Q0RQ-5VzM8";
+const API_KEY = "your-api-key";
 
 // Function to handle HTTP GET requests
 async function httpGet(endpoint, params = {}) {
